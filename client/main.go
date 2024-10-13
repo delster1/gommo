435022f47885ba663d6d1ded50cd9ab519592116
delster1 <delsterone@gmail.com>
2024-10-13 17:33:02 -0500

    simple client server working, next sending map back and forth

--- client/main.go
+++ client/main.go
@@ -13,12 +13,42 @@ func main() {
         return
     }
 
-    // Send some data to the server
-    _, err = conn.Write([]byte("Hello, server!"))
+    defer conn.Close()
+
+    _, err = conn.Write([]byte("gommo"))
     if err != nil {
         fmt.Println(err)
         return
     }
+    buf := make([]byte, 1024)
+
+    n, err := conn.Read(buf)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    sessionID := string(buf[:n])
+    fmt.Printf("Received SessionID: %s\n", sessionID)
+
+    for {
+        // MARK: client Loop
+        // this is where operations from the client are sent and where we will draw the window and stuff
+        n, err := conn.Read(buf)
+        if err != nil {
+            fmt.Println(err)
+            return
+        
+        }
+        fmt.Printf("Received: %s\n", buf[:n])
+
+        response_str := sessionID + "\nresponse"
+        fmt.Println(response_str)
+
+    }
+
+
+    // Send some data to the server
+
 
     // Close the connection
     conn.Close()
--- server/main.go
+++ server/main.go
@@ -2,26 +2,57 @@ package main
 
 import (
     "fmt"
+	"bytes"
+	"encoding/hex"
+	"crypto/rand"
     "net"
+	"time"
 )
 type Cell int 
 
 const (
-	StateEmpty Cell = iota
+	StateEmpty Cell = iota // makes stateEmpty = 0 , player = 1, statefood = 3, 
 	StatePlayer
 	StateFood
 )
 type universe struct {
 	Map []Cell
+	Width int
+	Height int
 }
-
-func create_universe() (u universe) {
-	u = universe{Map: nil}
-	u.Map = make([]Cell, 100)
+func create_universe(width int, height int) (u universe) {
+	u = universe{Map: nil, Width : width, Height : height }
+	u.Map = make([]Cell, width * height)
 	return u
 } 
+func GenerateSessionID(length int) (string, error) {
+	// Create a byte slice with the specified length
+	bytes := make([]byte, length)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		return "", err // Return an error if random generation fails
+	}
+	
+	// Return the session ID as a hex string
+	return hex.EncodeToString(bytes), nil
+}
+func iterate_over_cells(u universe) (new_u universe) {
+	for i := range u.Map {
+		// y := i % width
+		// x := i % height
+		
+		new_u.Map[i] = 0
+
+
+	}
+	return new_u
+}
 
 func main() {
+	
+	server_u := create_universe(50, 50)
+
+	fmt.Println(server_u)
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		fmt.Println(err)
@@ -30,6 +61,7 @@ func main() {
 	defer ln.Close()
 
 	for {
+		//MARK: server loop
 		conn, err := ln.Accept()
 		if err != nil {
 			fmt.Println(err)
@@ -38,19 +70,44 @@ func main() {
 		
 		go handleConnection(conn)
 	}
-
 }
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-
 	buf := make([]byte , 1024)
-	_, err := conn.Read(buf)
+	n, err := conn.Read(buf)
 	if err != nil {
-        fmt.Println(err)
-        return
-    }
+		fmt.Println(err)
+		return
+	}
 
-    // Print the incoming data
-    fmt.Printf("Received: %s", buf)
+	// Print the incoming data
+	fmt.Printf("Received: %s\n", buf[:n])
+	if !bytes.Equal(buf[:n], []byte("gommo")) {
+		fmt.Println("Invalid request")
+		conn.Close()
+	}
+	sessionID, _ := GenerateSessionID(16)
+	conn.Write([]byte(sessionID))
+	for {
+		
+		
+		response := process_request(buf)	
+		if response != nil {
+			_, err := conn.Write([]byte(response))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	}
+
+}
+
+func process_request(buf []byte ) (response []byte) {
+	switch {
+	case bytes.Equal(buf, []byte("gommo")):
+		fmt.Println("Incoming client, generating session id")
+	}
+	return nil
 }
